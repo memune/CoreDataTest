44ebf75b04f152969671ff9edc3c228c54b88721
Ted Paark <memuune@gmail.com>
2022-02-22 19:37:23 +0900

    memo first version

--- CoreDataTest/ContentView.swift
+++ CoreDataTest/ContentView.swift
@@ -1,88 +1,47 @@
-//
-//  ContentView.swift
-//  CoreDataTest
-//
-//  Created by Taehoon Park on 2022/02/22.
-//
-
 import SwiftUI
 import CoreData
 
 struct ContentView: View {
+    
     @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
+    @FetchRequest(entity: Animals.entity(), sortDescriptors: []) var animals: FetchedResults<Animals>
+    @State private var newAnimalName = ""
+    
     var body: some View {
         NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
+            VStack {
+                HStack {
+                    TextField("Add New", text: self.$newAnimalName)
+                    Button("Save") {self.contentSave()}
+                }
+                .padding(EdgeInsets(top: 10, leading: 30, bottom: 10, trailing: 30))
+                
+                
+                List {
+                    ForEach(animals, id: \.self) { animal in
+                        Text("동물 이름은 \(animal.name!)")
+                            .onTapGesture {
+                                self.newAnimalName = animal.name!
+                            }
+                    }
+                    .onDelete { IndexSet in
+                        for index in IndexSet {
+                            self.viewContext.delete(self.animals[index])
+                            try? self.viewContext.save()
+                        }
                     }
                 }
-                .onDelete(perform: deleteItems)
             }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
+            .navigationTitle("Inbox")
         }
     }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
+        
+        
+    func contentSave() {
+        let newAnimal = Animals(context: viewContext)
+        newAnimal.name = newAnimalName
+        try? self.viewContext.save()
     }
 }
+ 
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
-    }
-}
